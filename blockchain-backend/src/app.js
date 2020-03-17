6e14a9fe29625f19833d77f4717982814b6a1c5c
Sharad Shinde <shindesharad71@gmail.com>
2020-03-17 18:21:03 +0530

    Added: New API urls

--- blockchain-backend/src/app.js
+++ blockchain-backend/src/app.js
@@ -5,28 +5,21 @@ const cors = require('cors');
 const Web3 = require('web3');
 
 require('dotenv').config();
-
 const middlewares = require('./middlewares');
-const api = require('./api');
-
 const app = express();
 
 app.use(morgan('dev'));
 app.use(helmet());
 app.use(cors());
 
-const web3Client = new Web3(Web3.givenProvider || "ws://localhost:7545");
 
-console.log(web3Client);
-
+const web3Client = new Web3(new Web3.providers.HttpProvider(process.env.NETWORK_URL || "ws://localhost:7545"));
 
 app.get('/', (req, res) => {
   res.json({
     message: '🦄🌈✨👋🌎🌍🌏✨🌈🦄'
   });
 });
-
-app.use('/api/v1', api);
 
 app.use(middlewares.notFound);
 app.use(middlewares.errorHandler);
