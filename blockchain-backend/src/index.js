40e8a0fbe0ee8ba8b1163d223987a6cc24d39247
Sharad Shinde <shindesharad71@gmail.com>
2020-03-17 18:04:26 +0530

    Added: env

--- blockchain-backend/src/index.js
+++ blockchain-backend/src/index.js
@@ -2,7 +2,5 @@ const app = require('./app');
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
-  /* eslint-disable no-console */
-  console.log(`Listening: http://localhost:${port}`);
-  /* eslint-enable no-console */
+  console.info(`Listening: http://localhost:${port}`);
 });
