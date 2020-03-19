70af2ec77c2c2195f0870d3a24c75360cccb3e27
Sharad Shinde <shindesharad71@gmail.com>
2020-03-19 09:25:58 +0530

    Added Css lib

--- frontend/src/app/app.service.ts
+++ frontend/src/app/app.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AppService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  createAccount() {
+    return this.http.get(`${environment.BASE_URL}account`);
+  }
+
+  getBalance() {
+    const userId = window.localStorage.getItem('userId');
+    if (userId) {
+      return this.http.get(`${environment.BASE_URL}balance/${userId});
+    } else {
+      return false;
+    }
+  }
 }
